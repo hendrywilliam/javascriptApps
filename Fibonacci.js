93fdaacf6bf7ffbb2ca3d821b1b7f0c25a74a604
hendrywilliam <hendriwilliam29@gmail.com>
2022-10-08 13:41:34 +0700

    Fibonacci number, result in array

---
+++ Fibonacci.js
@@ -0,0 +1,12 @@
+const fibonacciArr = [];
+
+const fibonacci = (n) => {
+    if(n <= 1) return n;
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+let range = 10;
+for(let i = 0; i<range; i++) {
+    fibonacciArr.push(fibonacci(i))
+}
+console.log(fibonacciArr);
