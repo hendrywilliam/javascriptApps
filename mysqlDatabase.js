e8d75b91b775bc02762372138c2aeba709af7fc4
hendrywilliam <hendriwilliam29@gmail.com>
2022-10-10 18:30:32 +0700

    Get all datas in mysql database

---
+++ mysqlDatabase.js
@@ -0,0 +1,20 @@
+//mysql -> npm install mysql
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "db"
+})
+
+//lets assume that you've created database and it's own table (users)
+con.connect(err => {
+    if(err) throw err;
+    console.log('Connected to database');
+    const sql = 'SELECT * FROM users';
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
